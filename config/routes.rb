6fe642a1b0fb0c9af37029fbeb03fd7b6444232d
KumarAmitt <amitt2015@outlook.com>
2021-02-10 15:47:10 +0530

    Resourceful Routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   # get 'articles/index'
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
